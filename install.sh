a09df8960a73d58453df33f1926049cc56406ac2
Yuxuan Chen <yuxuanchen1997@outlook.com>
2015-10-28 15:48:25 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+cd
+rm -rf .bashrc .vimrc
+wget https://raw.githubusercontent.com/yuxuanchen1997/mydotfiles/master/.vimrc
+wget https://raw.githubusercontent.com/yuxuanchen1997/mydotfiles/master/.bashrc
