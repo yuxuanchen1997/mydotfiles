93af9bad9025a4426515133ae6a329c8add5f5d5
Yuxuan Chen <yuxuanchen1997@outlook.com>
2016-02-08 21:18:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 cd
 wget https://raw.githubusercontent.com/yuxuanchen1997/mydotfiles/master/.vimrc -O .vimrc
 wget https://raw.githubusercontent.com/yuxuanchen1997/mydotfiles/master/.bashrc -O .bashrc
-bash
