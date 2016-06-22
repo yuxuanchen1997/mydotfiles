b37dacdd35263f379bb3044241b14c94be8de66c
Yuxuan Chen <yuxuanchen1997@outlook.com>
2016-06-22 17:25:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 cd
 wget https://raw.githubusercontent.com/yuxuanchen1997/mydotfiles/master/.vimrc -O .vimrc
 wget https://raw.githubusercontent.com/yuxuanchen1997/mydotfiles/master/.bashrc -O .bashrc
+echo "All done. Have fun!"
